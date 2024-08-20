66e9710db878433c8066f9b81115ec0b48432d3e
Eswar Krishna Maganti <eswarmaganti@Eswars-MacBook-Air.local>
2024-08-20 17:57:28 +0530

    updated deployment ansible playbook

--- jenkins/dev/src/main.py
+++ jenkins/dev/src/main.py
@@ -5,7 +5,7 @@ import os
 
 home_dir = '/opt/SP/cicd-tasks-app'
 playbook_path = f"{home_dir}/jenkins/dev/src/playbooks/dev-deploy.yaml"
-inventory_path = f"{home_dir}/jenkins/dev/sec/config/hosts"
+inventory_path = f"{home_dir}/jenkins/dev/src/config/hosts"
 play_command = shlex.split(f''' ansible-playbook -i {inventory_path} {playbook_path} ''')
 
 res = subprocess.run(play_command)
